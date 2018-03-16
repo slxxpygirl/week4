a44af09840840d155dda9199dc8eefd4ae8a85ea
Adam Zheng <Venator-Fox@users.noreply.github.com>
2018-03-15 19:50:51 -0500

    Update install to use

--- 1.0.0/install.sh
+++ 1.0.0/install.sh
@@ -3,8 +3,11 @@
 #Default runtime variable if none is supplied
 HELLO_WORLD_STATEMENT=${HELLO_WORLD_STATEMENT:='You are lazy since you are using the default and did not specify this variable.'}
 
-echo "You said:"
-echo $HELLO_WORLD_STATEMENT
+PATH="$PATH:/usr/games"
+export PATH
+
+fortune | cowsay -f dragon-and-cow
+cowsay $HELLO_WORLD_STATEMENT
 
 #Self Destruct
 rm -- "$0" && exit 0
